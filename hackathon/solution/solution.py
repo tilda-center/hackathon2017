d54a27c304d30f882168f5a52bceeb288e589582
Goran Mekić <meka@tilda.center>
2017-11-04 14:14:57 +0100

    More pseudo code

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -6,29 +6,74 @@ from hackathon.utils.utils import ResultsMessage, DataMessage, PVMode, \
 from hackathon.framework.http_server import prepare_dot_dir
 
 
-def worker(msg: DataMessage) -> ResultsMessage:
+def prodaj(msg):
     load_one = True
     load_two = True
     load_three = True
     power_reference = 0.0
     pv_mode = PVMode.ON
+    result = ResultsMessage(
+        data_msg=msg,
+        load_one=load_one,
+        load_two=load_two,
+        load_three=load_three,
+        power_reference=power_reference,
+        pv_mode=pv_mode,
+    )
+    return result
 
-    # if overload baterije u minimum, gasi bojler
-    if msg.bessSOC < 0.1:
-        load_three = False
 
-    # if overload baterije u maximum, prodaj struju ako je povoljno inače pali bojler
-    # if 30% bojlera skuplje od 0.1, gasi bojler inače upali
-    # if nema mrežne struje, gasi bojler
-    # if baterija ispod X kWh, gasi bojler
-    # ako je penal bojlera manji od prodajne cene, gasi bojler i prodaj višak ili puni bateriju
+def stedi(msg):
+    load_one = True
+    load_two = True
+    load_three = True
+    power_reference = 0.0
+    pv_mode = PVMode.ON
+    result = ResultsMessage(
+        data_msg=msg,
+        load_one=load_one,
+        load_two=load_two,
+        load_three=load_three,
+        power_reference=power_reference,
+        pv_mode=pv_mode,
+    )
+    return result
+
+
+def potrosiIliProdaj(msg):
+    load_one = True
+    load_two = True
+    load_three = True
+    power_reference = 0.0
+    pv_mode = PVMode.ON
+    result = ResultsMessage(
+        data_msg=msg,
+        load_one=load_one,
+        load_two=load_two,
+        load_three=load_three,
+        power_reference=power_reference,
+        pv_mode=pv_mode,
+    )
+    return result
+
+
+def worker(msg: DataMessage) -> ResultsMessage:
+    result = None
+    # overload baterije
+    if msg.bessOverload:
+        # maximum
+        if msg.bessSOC == 1:
+            result = prodaj(msg)
+        else:
+            result = stedi(msg)
+    elif not msg.grid_status:
+        result = stedi(msg)
+    else: # sve normalno radi
+        result = potrosiIliProdaj(msg)
+
+
     # ako puno raste PV, biće puno energije
-    return ResultsMessage(data_msg=msg,
-                          load_one=load_one,
-                          load_two=load_two,
-                          load_three=load_three,
-                          power_reference=power_reference,
-                          pv_mode=pv_mode)
+    return result
 
 
 def run(args) -> None:
