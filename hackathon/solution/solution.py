fa51499569a753111c9e99928d91cedd42738569
Goran Mekić <meka@tilda.center>
2017-11-04 13:24:01 +0100

    Pseudo code

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -7,20 +7,28 @@ from hackathon.framework.http_server import prepare_dot_dir
 
 
 def worker(msg: DataMessage) -> ResultsMessage:
-    """TODO: This function should be implemented by contestants."""
-    # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
-
-    # Dummy result is returned in every cycle here
     load_one = True
     load_two = True
     load_three = True
-    import ipdb; ipdb.set_trace()
+    power_reference = 0.0
+    pv_mode = PVMode.ON
+
+    # if overload baterije u minimum, gasi bojler
+    if msg.bessSOC < 0.1:
+        load_three = False
+
+    # if overload baterije u maximum, prodaj struju ako je povoljno inače pali bojler
+    # if 30% bojlera skuplje od 0.1, gasi bojler inače upali
+    # if nema mrežne struje, gasi bojler
+    # if baterija ispod X kWh, gasi bojler
+    # ako je penal bojlera manji od prodajne cene, gasi bojler i prodaj višak ili puni bateriju
+    # ako puno raste PV, biće puno energije
     return ResultsMessage(data_msg=msg,
                           load_one=load_one,
                           load_two=load_two,
                           load_three=load_three,
-                          power_reference=0.0,
-                          pv_mode=PVMode.ON)
+                          power_reference=power_reference,
+                          pv_mode=pv_mode)
 
 
 def run(args) -> None:
