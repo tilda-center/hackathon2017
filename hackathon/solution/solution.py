352a9a4ccef9ea6e359e0784eac663f55cda570c
Goran Mekić <meka@tilda.center>
2017-11-04 16:25:07 +0100

    Handle overload

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -12,6 +12,37 @@ def potrosi(msg):
     load_three = True
     power_reference = 0.0
     pv_mode = PVMode.ON
+
+    extra_production = msg.solar_production - msg.current_load
+    if msg.grid_status: # radi elektrovojvodina
+        if extra_production < 0: # pravim manje energije od max load
+            if msg.solar_production + 6.0 < msg.current_load: # panel i baterija prave manje energije od max load
+                power_reference = 6.0
+                if msg.buying_price / 60 >= 0.1: # više se isplati gasiti load3 nego kupovati
+                    load_three = False
+            else: # panel i baterija mogu da prave makar max load
+                if extra_production < -6.0:
+                    power_reference = 6.0
+                else:
+                    power_reference = -extra_production
+        else: # pravim barem energije od max load
+            power_reference = 0.000001
+    else: # ne radi elektrovojvodina
+        if extra_production >= 0: # pravim iz panela barem max load
+            pv_mode = PVMode.OFF
+            if msg.current_load * 0.7 >= 6.0: # isklučenje load3 nije dovoljno
+                load_two = False
+            else: # isključenje load3 je dovoljno
+                load_three = False
+        else: # pravim iz panela manje od max load
+            if msg.solar_production + 6.0 <= msg.current_load: # panel i baterija ne prave dovoljno
+                current_load = msg.current_load * 0.7
+                if msg.solar_production + 6.0 < current_load: # panel i baterija prave manje od load1 + load2
+                    load_two = False # tloken
+                else: # panel i baterija prave barem load1 + load2
+                    load_three = False
+
+
     result = ResultsMessage(
         data_msg=msg,
         load_one=load_one,
